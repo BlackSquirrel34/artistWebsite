fc46f62a9b149238ffd21a7537c3f0d2b31b1dec
BlackSquirrel <Maexxla3458@proton.me>
2025-08-12 08:42:04 +0200

    Navbar needs no BASE_URL any more

--- website/src/components/PageNotFound.tsx
+++ website/src/components/PageNotFound.tsx
@@ -1,8 +1,4 @@
 import React from 'react'
-import Image from 'next/image'
-import { getImages } from '@/utils/apiMock'
-
-const images = getImages()
 
 export default function PageNotFound() {
   return <div>Page was not found. we found neither a slug nor global with this name. 404</div>
